3cbc412b87f1abad1d5ac4e1b32beb7569e97880
James Craig <admin@gutgames.com>
2013-05-08 21:55:27 -0400

    We've achieved automation... Sort of...
    Still need to set up the nightly build but the automatic version update
    is there. However I still need to update dependency versions based on
    packages.config file.

---
+++ NuspecUpdater/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Utilities.IO.ExtensionMethods;
+
+namespace NuspecUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (FileInfo File in new DirectoryInfo("..\\..\\..\\BatmanPackages\\").EnumerateFiles("*.nuspec", SearchOption.AllDirectories))
+            {
+                string FileContents = File.Read();
+                Match VersionMatch = Regex.Match(FileContents, "<version>(?<VersionNumber>.*)</version>");
+                string[] VersionInfo = VersionMatch.Groups["VersionNumber"].Value.Split('.');
+                string NewVersion = VersionInfo[0] + "." + VersionInfo[1] + ".";
+                if (VersionInfo.Length > 2)
+                    NewVersion += (int.Parse(VersionInfo[2]) + 1).ToString("D4");
+                else
+                    NewVersion += "0001";
+                File.Save(Regex.Replace(FileContents, "<version>(?<VersionNumber>.*)</version>", "<version>" + NewVersion + "</version>"));
+            }
+            new FileInfo("..\\..\\..\\BatmanPackages\\Package.bat").Execute();
+            foreach (FileInfo File in new DirectoryInfo("..\\..\\..\\BatmanPackages\\").EnumerateFiles("*.nupkg", SearchOption.AllDirectories))
+            {
+                File.Delete();
+            }
+        }
+    }
+}
