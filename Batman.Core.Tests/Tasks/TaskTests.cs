5ac46a2c78a1ca5faef3aabd35653b9d7bc2a608
James Craig <admin@gutgames.com>
2014-06-13 12:32:25 -0400

    Updated packages

--- Batman.Core.Tests/Tasks/TaskTests.cs
+++ Batman.Core.Tests/Tasks/TaskTests.cs
@@ -1,19 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using Xunit;
 using Batman.Core.FileSystem;
 using Batman.Core.FileSystem.Interfaces;
-using System.IO;
-
-using Utilities.DataTypes.ExtensionMethods;
-using Batman.Core.Tasks.Interfaces;
-using Batman.Core.Tasks.Enums;
 using Batman.Core.Tasks;
+using Batman.Core.Tasks.Enums;
+using Batman.Core.Tasks.Interfaces;
+using Utilities.DataTypes.ExtensionMethods;
+using Xunit;
 
 namespace Batman.Core.Tests.Tasks
 {
+    public class SuperTask : ITask
+    {
+        public string Name
+        {
+            get { return "SuperTask"; }
+        }
+
+        public Core.Tasks.Enums.RunTime TimeToRun
+        {
+            get { return RunTime.PreStart; }
+        }
+
+        public void Run()
+        {
+        }
+    }
+
     public class TaskTests
     {
         [Fact]
@@ -21,25 +37,7 @@ namespace Batman.Core.Tests.Tasks
         {
             TaskManager Manager = new TaskManager();
             Assert.DoesNotThrow(() => Manager.Run(RunTime.PreStart));
-            Assert.Equal(1, Manager.Tasks.Count);
-        }
-    }
-
-    public class SuperTask : ITask
-    {
-        public Core.Tasks.Enums.RunTime TimeToRun
-        {
-            get { return RunTime.PreStart; }
-        }
-
-        public string Name
-        {
-            get { return "SuperTask"; }
-        }
-
-        public void Run()
-        {
-            
+            Assert.Equal(2, Manager.Tasks.Count);
         }
     }
 }
